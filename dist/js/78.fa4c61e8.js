25af491735c2f9307be6580c7c743db2ac816574
Frey <tanmaybajaj567@gmail.com>
2022-10-02 15:33:49 +0530

    hdqwuhd

---
+++ dist/js/78.fa4c61e8.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkQRCodeScanner"]=self["webpackChunkQRCodeScanner"]||[]).push([[78],{6078:function(e,n,t){t.r(n),t.d(n,{startStatusTap:function(){return a}});var r=t(65),o=t(4074),s=t(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const n=e.innerWidth,t=e.innerHeight,a=document.elementFromPoint(n/2,t/2);if(!a)return;const c=(0,o.a)(a);c&&new Promise((e=>(0,s.c)(c,e))).then((()=>{(0,r.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,o.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.fa4c61e8.js.map
